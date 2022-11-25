0bbbd062bb768d05270146179d07e35b5f89cb29
“JWsley” <jonathanwleycijani@gmail.com>
2022-11-25 00:40:55 -0300

    first commit

---
+++ pages/carousel.js
@@ -0,0 +1,37 @@
+// slider do banner====================
+
+
+var slider = tns({
+    container:'.banner',
+    items:1,
+    autoplay:true,
+    mouseDrag:true,
+    controls:false,
+    speed:1000,
+    autoplayButtonOutput:false,
+    nav:true,
+    mode:'carousel',
+    navPosition:"bottom",
+  
+})
+
+// =====================================================
+
+// slider do carousel2====================
+
+
+var slider2 = tns({
+
+
+
+    container:'.carousel2',
+    items:5,
+    controls:true,
+    loop:false,
+    mouseDrag:false,
+    controlsContainer:'.controls',
+    nav:false
+    
+
+
+})
---
+++ pages/page1.js
@@ -0,0 +1,16 @@
+function activeSearch(){
+    var bar = document.querySelector('#pesquisa')
+    var input = document.querySelector('#input')
+
+
+    bar.style.borderBottom= "3px solid #BCECE0";
+    input.style.opacity= "1";
+    input.style.cursor= "pointer";
+
+
+}
+
+
+
+
+ 
